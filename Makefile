cd43f06aa6a01d9329747b94d6d6f4ca6ec0a12c
Ali Sina Sultani <alisinasultani255@gmail.com>
2025-10-03 23:52:33 +0430

    custom middleware and logs

---
+++ Makefile
@@ -0,0 +1,27 @@
+build:
+	docker compose -f local.yaml up --build -d --remove orphans
+
+up:
+	docker compose -f local.yaml up -d
+
+down:
+	compose compose -f local.yaml down
+
+down-v:
+	docker compose -f local.yaml down -v
+
+show-logs:
+	docker compose -f local.yaml logs
+
+show-logs-api:
+	docker compose -f local.yaml logs api
+
+show-logs-client:
+	docker compose -f local.yaml logs client
+
+user:
+	docker run --rm mern-invoice-api whoami
+volume:
+	docker volume inspect mern-invoice_mongod-data
+
+
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import { morganMiddleware, systemLogs } from "./utils/Logger.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ if (process.env.NODE_ENV == "development") {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(morganMiddleware);
 
 app.get("/api/v1/test", (req, res) => {
   res.json({ Hi: "Welcome to the invoice app" });
@@ -21,5 +23,8 @@ const PORT = process.env.PORT || 1997;
 app.listen(PORT, () => {
   console.log(
     `server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
+  systemLogs.info(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
   );
 });
